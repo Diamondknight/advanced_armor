b0f0500c3c9d63b228a48fd3e9279fc06842d874
Ferrum Princeps I <ferrumprinceps@politician.com>
2018-05-14 14:46:38 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,21 +4,23 @@ function armor.add(name, def) -- or any other name
         -- some fields...
         inventory_image = def.texture, -- or any other name, again
         ---armor groups
-        slash_armor = def.slash_armor,
-        stab_armor = def.stab_armor,
-        peirce_armor = def.peirce_armor,
+        groups = {slash_armor = def.slash_armor,
+        hack_armor = def.hack_armor,
+        pierce_armor = def.pierce_armor,
         blunt_armor = def.blunt_armor,
-        ballistic_armor = def.slash_armor,
+        ballistic_armor = def.ballistic_armor,
         cracky_armor = def.cracky_armor,
         crumbly_armor = def.crumbly_armor,
         choppy_armor = def.choppy_armor,
         snappy_armor = def.snappy_armor,
-        fire_armor = def.fire_armor,
         water_armor = def.water_armor,
         radiation_armor = def.radiation_armor,
-        ice_armor = def.ice_armor,
+        cold_armor = def.cold_armor,
+        heat_armor = def.heat_armor,
         ---armor weight
         armor_weight_walk = def.armor_weight_walk,
-        armor_weight_jump = def.armor_weight_jump,
+        armor_weight_jump = def.armor_weight_jump},
+        --durability
+        uses = def.durability,
     })
 end
