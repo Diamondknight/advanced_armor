033fc788e3359c9fb8c050f24e22f026587a3ce3
Ferrum Princeps I <ferrumprinceps@politician.com>
2018-05-15 10:57:45 -0500

    Update weapon_overrides.lua

--- weapon_overrides.lua
+++ weapon_overrides.lua
@@ -5,7 +5,7 @@ minetest.register_tool(":default:sword_wood", {
 	description = "Wooden Sword",
 	inventory_image = "default_tool_woodsword.png",
 	tool_capabilities = {
-		full_punch_interval = 1,
+		full_punch_interval = 0.6,
 		max_drop_level=0,
 		groupcaps={
 			snappy={times={[2]=1.6, [3]=0.40}, uses=10, maxlevel=1},
@@ -25,7 +25,7 @@ minetest.register_tool(":default:sword_stone", {
 		groupcaps={
 			snappy={times={[2]=1.4, [3]=0.40}, uses=20, maxlevel=1},
 		},
-		damage_groups = {fleshy=6, slash=5, hack=3, pierce=4, blunt=2, crumbly=1, choppy=1, snappy=6},
+		damage_groups = {fleshy=6, slash=5, hack=3, pierce=4, blunt=2, crumbly=1, choppy=1, snappy=7},
 	},
 	sound = {breaks = "default_tool_breaks"},
 })
@@ -34,12 +34,12 @@ minetest.register_tool(":default:sword_bronze", {
 	description = "Bronze Sword",
 	inventory_image = "default_tool_bronzesword.png",
 	tool_capabilities = {
-		full_punch_interval = 0.8,
+		full_punch_interval = 1.4,
 		max_drop_level=1,
 		groupcaps={
 			snappy={times={[1]=2.75, [2]=1.30, [3]=0.375}, uses=25, maxlevel=2},
 		},
-		damage_groups = {fleshy=8, slash=7, hack=5, pierce=6, blunt=3, crumbly=1, choppy=1, snappy=8},
+		damage_groups = {fleshy=8, slash=7, hack=5, pierce=6, blunt=4, crumbly=1, choppy=1, snappy=9},
 	},
 	sound = {breaks = "default_tool_breaks"},
 })
@@ -48,12 +48,12 @@ minetest.register_tool(":default:sword_steel", {
 	description = "Steel Sword",
 	inventory_image = "default_tool_steelsword.png",
 	tool_capabilities = {
-		full_punch_interval = 0.8,
+		full_punch_interval = 1.3,
 		max_drop_level=1,
 		groupcaps={
 			snappy={times={[1]=2.5, [2]=1.20, [3]=0.35}, uses=30, maxlevel=2},
 		},
-		damage_groups = {fleshy=8, slash=7},
+		damage_groups = {fleshy=8, slash=7, hack=5, pierce=6, blunt=3, crumbly=1, choppy=1, snappy=9},
 	},
 	sound = {breaks = "default_tool_breaks"},
 })
@@ -62,12 +62,12 @@ minetest.register_tool(":default:sword_mese", {
 	description = "Mese Sword",
 	inventory_image = "default_tool_mesesword.png",
 	tool_capabilities = {
-		full_punch_interval = 0.7,
+		full_punch_interval = 1.3,
 		max_drop_level=1,
 		groupcaps={
 			snappy={times={[1]=2.0, [2]=1.00, [3]=0.35}, uses=30, maxlevel=3},
 		},
-		damage_groups = {fleshy=8, slash=8},
+		damage_groups = {fleshy=9, slash=8, hack=6, pierce=7, blunt=3, crumbly=1, choppy=1, snappy=10},
 	},
 	sound = {breaks = "default_tool_breaks"},
 })
@@ -76,12 +76,12 @@ minetest.register_tool(":default:sword_diamond", {
 	description = "Diamond Sword",
 	inventory_image = "default_tool_diamondsword.png",
 	tool_capabilities = {
-		full_punch_interval = 0.7,
+		full_punch_interval = 1.3,
 		max_drop_level=1,
 		groupcaps={
 			snappy={times={[1]=1.90, [2]=0.90, [3]=0.30}, uses=40, maxlevel=3},
 		},
-		damage_groups = {fleshy=10, slash=9},
+		damage_groups = {fleshy=10, slash=9, hack=7, pierce=6, blunt=3, crumbly=1, choppy=1, snappy=11},
 	},
 	sound = {breaks = "default_tool_breaks"},
 })
