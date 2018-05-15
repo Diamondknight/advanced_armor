0f1fed0b7a8d5590701958ab19acfe9467a99dfd
Ferrum Princeps I <ferrumprinceps@politician.com>
2018-05-15 11:53:17 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+dofile(minetest.get_modpath("advanced_armor") .. "/weapon_overrides.lua")
+
 --------------------------------------------API FOR NEW ARMORS----------------------------------------
 function armor.add(name, def) -- or any other name
     minetest.register_tool(name, { -- or craftitem
@@ -28,3 +30,28 @@ function armor.add(name, def) -- or any other name
         uses = def.durability,
     })
 end
+
+function 
+if minetest.get_modpath("placeholderforgeroldsmodname") = false then
+sfinv.register_page("advanced_armor:armor", {
+	title = "Armor",
+	get = function(self, player, context)
+		return sfinv.make_formspec(player, context, [[
+				list[current_player;craft;1.75,0.5;3,3;]
+				list[current_player;craftpreview;5.75,1.5;1,1;]
+				image[4.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]
+				listring[current_player;main]
+				listring[current_player;craft]
+				image[0,4.75;1,1;gui_hb_bg.png]
+				image[1,4.75;1,1;gui_hb_bg.png]
+				image[2,4.75;1,1;gui_hb_bg.png]
+				image[3,4.75;1,1;gui_hb_bg.png]
+				image[4,4.75;1,1;gui_hb_bg.png]
+				image[5,4.75;1,1;gui_hb_bg.png]
+				image[6,4.75;1,1;gui_hb_bg.png]
+				image[7,4.75;1,1;gui_hb_bg.png]
+			]], true)
+	end
+})
+        end
+    end
